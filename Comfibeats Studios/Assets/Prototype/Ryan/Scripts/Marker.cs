99b6d260cd85e735a69c246ae84f548b6fa81880
Ryan <ryan_w2012@yahoo.com>
2023-04-28 17:01:55 -0700

    Marker test

--- Comfibeats Studios/Assets/Prototype/Ryan/Scripts/Marker.cs
+++ Comfibeats Studios/Assets/Prototype/Ryan/Scripts/Marker.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Marker : MonoBehaviour
 {
+    [SerializeField]
+    private bool start = false;
+
+    [SerializeField]
+    private GameObject highlight;
+    [SerializeField]
+    private Slider sliderLight;
+
+    [SerializeField]
+    private float radius;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +29,35 @@ public class Marker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (highlight != null && Input.GetKeyDown(KeyCode.Space)){
+            if (sliderLight != null)
+            {
+                sliderLight.value += .01f;
+            }
+        }
+        if(highlight != null && Input.GetKey(KeyCode.Space))
+        {
+            sliderLight.value += .01f;
+        }
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Testing");
+        if (other.tag == "Note")
+        {
+            highlight = other.gameObject;
+            if (highlight.GetComponent<Slider>())
+            {
+                sliderLight = highlight.GetComponent<Slider>();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
