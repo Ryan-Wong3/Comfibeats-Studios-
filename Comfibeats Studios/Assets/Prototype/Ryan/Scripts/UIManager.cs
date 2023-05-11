f05cf8043efd3531959a96b2eaf9242e99674bc7
Ryan <ryan_w2012@yahoo.com>
2023-05-11 01:23:08 -0700

    Exchange

--- Comfibeats Studios/Assets/Prototype/Ryan/Scripts/UIManager.cs
+++ Comfibeats Studios/Assets/Prototype/Ryan/Scripts/UIManager.cs
@@ -1,12 +1,26 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    /*
     [SerializeField]
     private Score playerScore;
+    */
+
+    [SerializeField]
+    private GameObject endScreen;
+    [SerializeField]
+    private TMP_Text perfectText;
+    [SerializeField]
+    private TMP_Text earlyText;
+    [SerializeField]
+    private TMP_Text lateText;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,25 +28,55 @@ public class UIManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
+        
+    }
+
+    public void DisplayUIScore()
+    {
+        //Setactive the screen 
+        endScreen.gameObject.SetActive(true);
+
+        //setting text for perfect
+        perfectText.text = EndScreen.getPerfectScore().ToString();
+
+        //set the rest of the player score reuslts to the end screen
+        
+        
 
     }
 
-    public void UpdateUIScore()
+    private void UpdatePerfectScore()
     {
-        //Update the UI to the new score 
+        EndScreen.setPerfectScore(1);
+    }
+
+    private void UpdateEarlyScore()
+    {
+        EndScreen.setEarlyScore(1);
+    }
+
+    private void UpdateLateScore()
+    {
+        EndScreen.setLateScore(1);
     }
 
     private void OnEnable()
     {
-        Actions.ScoreUIUpdate += UpdateUIScore;
+        Actions.ScoreUIUpdate += DisplayUIScore;
+        Actions.PerfectUI += UpdatePerfectScore;
+        Actions.EarlyUI += UpdateEarlyScore;
+        Actions.LateUI += UpdateLateScore;
     }
 
     private void OnDisable()
     {
-        Actions.ScoreUIUpdate -= UpdateUIScore;
+        Actions.ScoreUIUpdate -= DisplayUIScore;
+        Actions.PerfectUI -= UpdatePerfectScore;
+        Actions.EarlyUI -= UpdateEarlyScore;
+        Actions.LateUI -= UpdateLateScore;
     }
 
 }
