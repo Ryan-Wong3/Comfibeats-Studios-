4b419af0d4cc04ac35e3a24152c3695a58a3be7e
katsudoen <124406770+katsudoen@users.noreply.github.com>
2023-05-16 04:45:58 -0700

    Revert "pause menus, end screen"
    This reverts commit 83678c9182d713056e46f71c5618510c1ac94816.

--- Comfibeats Studios/Assets/Scripts/Pause.cs
+++ Comfibeats Studios/Assets/Scripts/Pause.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-<<<<<<< Updated upstream
     void PauseGame()
     {
         Time.timeScale = 0;
     }
     void ResumeGame()
-=======
-    public void PauseGame()
-    {
-        Time.timeScale = 0;
-    }
-    public void ResumeGame()
->>>>>>> Stashed changes
     {
         Time.timeScale = 1;
     }
