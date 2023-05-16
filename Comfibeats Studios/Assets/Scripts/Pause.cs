d6862655baa17a707993b4592b1066abd5b99ece
SKAOPC\Sirena K <sirkao322@hotmail.com>
2023-05-16 05:32:31 -0700

    polish

--- Comfibeats Studios/Assets/Scripts/Pause.cs
+++ Comfibeats Studios/Assets/Scripts/Pause.cs
@@ -7,9 +7,11 @@ public class Pause : MonoBehaviour
     public void PauseGame()
     {
         Time.timeScale = 0;
+        GameManager.Instance.audioSource.Pause();
     }
     public void ResumeGame()
     {
         Time.timeScale = 1;
+        GameManager.Instance.audioSource.Play();
     }
 }
