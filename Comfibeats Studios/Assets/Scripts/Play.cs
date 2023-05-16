9b5a8a7cc9f2115b1785df28969055a88020278e
katsudoen <124406770+katsudoen@users.noreply.github.com>
2023-05-16 00:44:09 -0700

    level select + pause menu

---
+++ Comfibeats Studios/Assets/Scripts/Play.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("level select");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("main menu");
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Gameplay2");
+    }
+}
